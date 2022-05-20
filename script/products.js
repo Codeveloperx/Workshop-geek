adfb825d79f5a16fa1fa9e18ddf1c4ad7a864048
Camilo Orozco HDZ <camilo.orozcohdz@gmail.com>
2022-05-20 17:33:18 -0500

    Carrito de compras

--- script/products.js
+++ script/products.js
@@ -11,3 +11,24 @@ document.addEventListener('DOMContentLoaded', async () => {
    showProductos(dataProducts, container)
 
 })
+
+
+
+
+
+
+
+
+
+
+// toast
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
